83244b7e6f9864002e4833f01b54353d26deec17
err0rgod <ndgaming458@gmail.com>
2025-10-01 18:13:34 +0530

    done for even odd

---
+++ evenodd.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+
+    cin >> a;
+
+    if (a%2 == 0){
+        cout<<"the number " << a << " is even"<<endl;
+
+    }
+    else{
+        cout<<"the number "<< a<< " is odd"<<endl;
+    }
+}
