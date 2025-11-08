6ab2db17061bcc9caddb6982eb2bf509f74a9d76
err0rgod <ndgaming458@gmail.com>
2025-11-08 11:45:49 +0530

    start of the day

---
+++ patterns.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void pattern1(int n){
+    
+}
+
+int main(){
+    int t;
+    cin>>t;
+    for(int i=0;i<=t;i++){
+        int n;
+        cin>>n;
+        pattern1(n)
+    }
+}
