c388a017acdef65785065c6e746ca0d23678dc27
err0rgod <ndgaming458@gmail.com>
2025-11-08 14:47:38 +0530

    wowo

--- pattern.py
+++ pattern.py
@@ -2,8 +2,13 @@
 n=int(input("Enter the number "))
 
 for i in range(n,0,-1):
-    for j in range (i):
-        print (j+1, end=" ")
+    for j in range (n-i):
+        print (" ", end=" ")
+    for k in range(2*i+1):
+        print("*",end=" ")
+
+    for x in range(n-i):
+        print(" ", end=" ")
         
 
     print()
