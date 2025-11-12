b7aeb7f9fef11ff49e4faf302f714ab3391cc1f1
err0rgod <ndgaming458@gmail.com>
2025-11-12 22:56:27 +0530

    fuck off

--- digits.cpp
+++ digits.cpp
@@ -4,22 +4,12 @@ using namespace std;
 
 int main(){
     int n= 384683;
-    int w=n;
-    int s ;
+ 
 
-    if (n == 0) s = 1;
-    n = abs(n); // handle negative numbers
+    while(n>0){
+        int q=n%10;
+        cout<<q;
+        n=n/n;
 
-    while (n > 0) {
-        n /= 10;
-        s++;
-    }
-    for(int i=s;i>s;i++){
-
-        int q = w%10;
-        int w = w/10;
-        cout<< q;
-
-    }
-    cout<<w%10;
+   }
 }
