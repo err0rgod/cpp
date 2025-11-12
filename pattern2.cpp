e6f90471e837a63d124a9ddd24ac1793279cf02c
err0rgod <ndgaming458@gmail.com>
2025-11-12 22:17:17 +0530

    don know

--- pattern2.cpp
+++ pattern2.cpp
@@ -3,10 +3,14 @@
 using namespace std;
 
 int main(){
-    for(int i=0; i >4; i++){
-        for(int j = 0 ; j >i; j++){
+    int n = 5;
+    for(int i=0; i <n; i++){
+        for(int j = 0 ; j <i; j++){
             cout<<("*");
 
+        }
+        for(int j = n-1;j<n-1;j--){
+            cout<<"x";
         }
         cout<<endl;
     }
