e6c17e66e6067b1d639a3cf951aefbe0f9434e11
err0rgod <ndgaming458@gmail.com>
2025-11-19 13:57:14 +0530

    NEW FORMAT

--- strings.cpp
+++ strings.cpp
@@ -2,70 +2,41 @@
 #include <string>
 using namespace std;
 
-int main() {
-	string a;
+int main()
+{
+    string a;
 
-    
-    string b; 
-    
+    string b;
 
+    cin >> a;
 
+    cin >> b;
 
+    // int x,y;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-    cin>>a;
-
-    cin>>b;
-
-
-
-    
-
-    //int x,y;
-    
-    
     int lena = a.size();
-    
+
     int lenb = b.size();
-    
-    string c = a+b;
-    
+
+    string c = a + b;
+
     char anot = a[0];
     char bnot = b[0];
-    //anot = a[0];
-    //bnot = b[0];
-    
+    // anot = a[0];
+    // bnot = b[0];
+
     string anew;
     string bnew;
-    
+
     bnew = b;
     anew = a;
-    
-    anew[0]=b[0];
+
+    anew[0] = b[0];
     bnew[0] = a[0];
-    
-    
-    cout<<lena<<" "<<lenb<<endl;
-    cout<<c<<endl;
-    cout << anew<<" "<<bnew<<endl;
-    
-  
+
+    cout << lena << " " << lenb << endl;
+    cout << c << endl;
+    cout << anew << " " << bnew << endl;
+
     return 0;
 }
