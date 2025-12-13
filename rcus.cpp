c8c38a96b0eee40cd792f44e2f73122040564805
err0rgod <ndgaming458@gmail.com>
2025-12-13 12:33:26 +0530

    h

--- rcus.cpp
+++ rcus.cpp
@@ -3,21 +3,18 @@ int cnt = 0;
 void recurs(int i, int n)
 {
 
-
     if (n < i)
     {
         return;
-
     }
-    std::cout<< n << std::endl;
-
+    std::cout << n << std::endl;
     recurs(i, n - 1);
 }
 
 int main()
 {
     int n;
-    int i =1;
+    int i = 1;
     std::cin >> n;
     recurs(i, n);
 }
