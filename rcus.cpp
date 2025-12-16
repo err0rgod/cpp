176411aec9dc1ebf6b866d76072cdc3786298ba1
err0rgod <ndgaming458@gmail.com>
2025-12-17 04:29:52 +0530

    fucked up scene

--- rcus.cpp
+++ rcus.cpp
@@ -1,20 +1,43 @@
 #include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
 int n[5] = {1, 35, 2, 5, 6};
 
-int recurs(int n)
-{
 
-    if (n<=1) return n;
-
-    int last = recurs(n-1);
-    int slast = recurs(n-2);
-    
-    return last + slast;
-}
 int main()
 {
-    int n;
-    cin>>n;
-    cout<<recurs(n)<<endl;
+   
+    
+    unordered_map<int, int> mpp;
+    int arr[5] = {2,5,2,1,4};
+    for(int i = 0; i<5; i++){
+        
+        mpp[arr[i]++];
+
+    }
+
+    
+    int maxf = INT_MIN , minf = INT_MAX;
+
+    int maxelem= -1, minelem = -1 ;
+
+    for(auto &p : mpp){
+        int num = p.first;
+        int count  = p.second;
+
+        if(count > maxf){
+            maxf = count;
+            maxelem = num;
+
+        }
+        if(count < minf){
+            minf = count;
+            minelem = num;
+
+        }
+
+    }
+    cout<<"High freq element is : "<< maxelem << " and freq is : " << maxf<<endl;
+    cout<<"lowest one is : " << minelem << " and freq is : "<< minf<<endl;
+
 }
