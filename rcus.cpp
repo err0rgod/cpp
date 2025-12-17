10ce1aa8fba4e51e0c3b4ea18e562eb5e0059f1c
err0rgod <ndgaming458@gmail.com>
2025-12-17 14:59:24 +0530

    done init

--- rcus.cpp
+++ rcus.cpp
@@ -6,31 +6,20 @@ int main()
     int arr[5] = {23, 13, 4, 1, 5};
     // int arr[70] = {34, -12, 89, 45, 0, 67, -90, 23, 56, 12,99, -45, 18, 76, 3, -1, 54, 38, 91, 6,-33, 47, 82, 14, 5, 63, -70, 28, 39, 10,77, -8, 21, 60, 4, 88, -19, 31, 52, 7,95, -100, 26, 49, 11, 68, 2, -55, 80, 9,150, 200, -300, 400, 500, -600, 700, 800, 900, -1000,42, 73, -64, 85, 16, 27, 58, -91, 66, 19};
 
-    int a = INT_MAX;
-
-    int b = INT_MIN;
-    int min;
-    int max;
-    int c,d;
+    int a;
+    int x;
+    cin >> a;
 
     for (int i = 0; i < 5; i++)
     {
-        int num = arr[i];
-
-        if (num < a)
-        {
-            a = arr[i];
-            c = i;
-            min = num;
+        if (arr[i] == a)
+        { 
+            x = i;
+            
+            break;
         }
-        if (num > b)
-        {
-            b = arr[i];
-            max = num;
-            d= i;
-        }
-    }
-
-    cout << "minimum num is : " << min << endl;
-    cout << "maximum num is : " << max << endl;
+        else{
+            x = -1;
+        }    }
+    cout << "at index : " << x << " the value : " << a << " is present" << endl;
 }
