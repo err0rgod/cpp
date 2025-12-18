b97911f46a27f97087a61fb5417f756839c4d220
err0rgod <ndgaming458@gmail.com>
2025-12-18 18:42:04 +0530

    bubble sort

--- sorts.cpp
+++ sorts.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-void sort(int arr[], int n)
+void selection_sort(int arr[], int n)
 {
     for (int i = 0; i <= n - 2; i++)
     {
@@ -17,11 +17,20 @@ void sort(int arr[], int n)
     }
 }
 
+void bubble_sort(int arr[], int n){
+    for(int i = n-1; i>=1;i--){
+        for(int j = 0; j<=i-1; j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+}   
 int main()
 {
     int n = 6;
     int arr[6] = {13, 46, 24, 52, 10, 9};
-    sort(arr, n);
+    bubble_sort(arr, n);
     for (int i = 0; i < n; i++)
         cout << arr[i] << " ";
 }
