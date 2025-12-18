ee0432ab1e2df4e07c75477bd5d8a808255748ce
err0rgod <ndgaming458@gmail.com>
2025-12-18 19:01:03 +0530

    done with bubble sort

--- sorts.cpp
+++ sorts.cpp
@@ -26,6 +26,15 @@ void bubble_sort(int arr[], int n){
         }
     }
 }   
+
+void insertion_sort(int arr[], int n){
+    for(int i = 0;i<=n;i++){
+        for(int j = i;j>0 && arr[j-1]>arr[j];j--){
+            swap(arr[j-1],arr[j]);
+        }
+    }
+}
+
 int main()
 {
     int n = 6;
