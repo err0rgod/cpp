71b2345304e17cd9af8191fa8cd9e4bbe61da9f4
err0rgod <ndgaming458@gmail.com>
2025-12-22 13:54:26 +0530

    whoa another day another err0r

--- sorts.cpp
+++ sorts.cpp
@@ -18,8 +18,7 @@ void selection_sort(int arr[], int n)
     }
 }
 
-void bubble_sort(int arr[], int n)
-{
+void bubble_sort(int arr[], int n){
     for (int i = n - 1; i >= 1; i--)
     {
         for (int j = 0; j <= i - 1; j++)
