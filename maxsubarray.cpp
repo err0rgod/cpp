5b25ace8cabd69e367264ded0cd1d90be1f87745
err0rgod <ndgaming458@gmail.com>
2026-01-11 15:18:59 +0530

    woah

--- maxsubarray.cpp
+++ maxsubarray.cpp
@@ -2,52 +2,41 @@
 #include <math.h>
 using namespace std;
 
-
-int kadanes(int n,int arr[]){
+int kadanes(int n, int arr[])
+{
     int maxsum = 0;
     int a = 0;
-    for(int i =0;i<n;i++){
-        a+=arr[i];
+    for (int i = 0; i < n; i++)
+    {
+        a += arr[i];
 
+        maxsum = max(maxsum, a);
 
-
-
-
-
-        maxsum=max(maxsum,a);
-
-
-
-
-
-
-        
-        if(a<0) a = 0;
+        if (a < 0)
+            a = 0;
     }
     return maxsum;
 }
 
-
-int main(){
-    int n =9;
-    //int arr[6] = {1,2,3,4,5,6};
-    //int arr[7] = {3,-4,5,4,-1,7,-8};
-    int arr[9] = {1,8,6,2,5,4,8,3,7};
+int main()
+{
+    int n = 9;
+    // int arr[6] = {1,2,3,4,5,6};
+    // int arr[7] = {3,-4,5,4,-1,7,-8};
+    int arr[9] = {1, 8, 6, 2, 5, 4, 8, 3, 7};
     int maxsum = INT_MIN;
-    int a=0;
+    int a = 0;
 
-    for(int st = 0;st<n; st++){
+    for (int st = 0; st < n; st++)
+    {
         int cursum = 0;
-        for(int end= st;end<n;end++){
-            
+        for (int end = st; end < n; end++)
+        {
+
             cursum += arr[end];
-            maxsum = max(cursum,maxsum);
-            
-
+            maxsum = max(cursum, maxsum);
         }
-
-        
     }
-    cout<<"by regular method : "<<maxsum<<endl;
-    cout <<"By kadanes algo : "<< kadanes(n,arr)<<endl;
+    cout << "by regular method : " << maxsum << endl;
+    cout << "By kadanes algo : " << kadanes(n, arr) << endl;
 }
--- xor.cpp
+++ xor.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 
-int main(){
-    int a= 9 , b= 7, c =4;
-    int sum = a|b|c ;
+int main()
+{
+    int a = 9, b = 7, c = 4;
+    int sum = a | b | c;
 
-    std::cout<< sum ;
+    std::cout << sum;
 }
