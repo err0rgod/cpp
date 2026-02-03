190a5524c15285decf354be2d757c4f41e8118a8
err0rgod <ndgaming458@gmail.com>
2026-02-03 19:54:53 +0530

    hello

--- vectors.cpp
+++ vectors.cpp
@@ -1,26 +1,6 @@
 #include <iostream>
-#include <cmath>
-using namespace std;
-int main()
-{
-    int n = 4;
-    if (n < 2)
-    {
-        cout << "not prime\n";
-        return 0;
-    }
+#include <bits/stdc++.h>
 
-    bool isPrime = true;
-    int limit = static_cast<int>(sqrt(n));
-    for (int i = 2; i <= limit; ++i)
-    {
-        if (n % i == 0)
-        {
-            isPrime = false;
-            break;
-        }
-    }
-
-    cout << (isPrime ? "prime\n" : "not prime\n");
-    return 0;
+int main(){
+    pair<int,int> pair1 = {1,1};
 }
