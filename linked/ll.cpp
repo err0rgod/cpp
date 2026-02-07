f0043394320e2f5f002d0f49db3813b4e0f7e4a5
err0rgod <ndgaming458@gmail.com>
2026-02-07 16:10:09 +0530

    linked_list_learn

---
+++ linked/ll.cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+
+public:
+    node(int data1)
+    {
+        data = data1;
+        next = nullptr;
+    }
+};
+class ll
+{
+    node *head;
+    node *tail;
+
+public:
+    ll()
+    {
+        head = tail = NULL;
+    }
+    void push_front(int val)
+    {
+        node *newnode = new node(val);
+        if (head == NULL)
+        {
+            head = tail = newnode;
+        }
+        else
+        {
+            newnode->next = head;
+            head = newnode;
+        }
+    }
+    void push_back(int val)
+    {
+        node *newnode = new node(val);
+        if (head == NULL)
+        {
+            head = tail = newnode;
+        }
+        else
+        {
+            tail->next = newnode;
+            tail = newnode;
+        }
+    }
+    void pop_front()
+    {
+        if (head == NULL)
+        {
+            cout << "List is already empty";
+            return;
+        }
+        node *temp = head;
+        head = head->next;
+        temp->next = NULL;
+        delete temp;
+    }
+    void pop_back()
+    {
+        node *temp = head;
+        while (temp)
+        {
+            if (temp->next == tail)
+            {
+                cout << "Deleting " << tail->data << endl;
+                delete tail;
+                tail = temp;
+                cout << " and tail is pointing to " << tail->data << endl;
+                temp->next = NULL;
+            }
+            else
+            {
+                temp = temp->next;
+            }
+        }
+    }
+    void insertll(int val,int index){
+        if(index <0) return;
+        if(index == 0) pushfront(val);
+        else{
+            
+        }
+    }
+    void printll()
+    {
+        node *temp = head;
+        while (temp)
+        {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+        cout << "\nLinked List printed succesfully";
+    }
+};
+
+int main()
+{
+    ll l1;
+    // vector<int> array = {1,2,4,2,3,5,3};
+
+    l1.push_front(1);
+    l1.push_front(2);
+    l1.push_front(4);
+    l1.push_front(9);
+    l1.push_back(67);
+    cout << " Insertion succesfull\n";
+    l1.pop_front();
+    l1.pop_back();
+    l1.printll();
+}
