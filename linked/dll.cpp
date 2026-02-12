cf470c04460337dbb8234c76317590f1dd0c28de
err0rgod <ndgaming458@gmail.com>
2026-02-12 19:58:04 +0530

    done

---
+++ linked/dll.cpp
@@ -0,0 +1,21 @@
+#include <iostream>   
+using namespace std;
+
+class Node(){
+public:
+    int val;
+    Node* next;
+    Node* prev;
+    Node(int data){
+        val = data;
+        prev =next = NULL;
+    }
+};
+class Doublell(){
+    Node* head;
+    Node* tail;
+};
+
+int main(){
+
+}
