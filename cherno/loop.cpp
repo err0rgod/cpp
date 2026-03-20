dd06329005939036f44bffb9efd829f51c409084
err0rgod <ndgaming458@gmail.com>
2026-03-20 23:16:04 +0530

    hello world

--- cherno/loop.cpp
+++ cherno/loop.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
-int main (){
-    int i =0;
-
-    for(;i<5;){
-        std::cout <<"Hello\n";
-        i++;
-    }
+long long rec(int n ){
+    if(n==1) return 1;
+    return n+rec(n-1);
+}
+int main(){
+    int n = 5;
+    std::cout << rec(52)<<std::endl;
 }
