0bf35add20465fb305a299c2576576c04789854a
err0rgod <ndgaming458@gmail.com>
2026-04-02 01:15:08 +0530

    hwhe

--- class/class.cpp
+++ class/class.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
-extern int x;
+
+
+class entity{
+public:
+    int x,y;
+    void print(){
+        std::cout << x << " "<< y<<std::endl;
+    }
+};
+
 int main(){
-    std::cout << x << std::endl;
+    entity e;
+    e.x = 1;
+    e.y = 2;
+
+    entity e2;
+    e2 = {3,5};
+
+    e.print();
+    e2.print();
 }
--- class/static.cpp
+++
@@ -1,3 +0,0 @@
-
-static int  x = 5;
- 
